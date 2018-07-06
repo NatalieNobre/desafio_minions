6f808ee69a58675dc796c6b6ddb776a729753d08
Natalie N. da Cunha <natalienobre@gmail.com>
2018-07-06 20:21:43 -0300

    Fix attempt heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module DesafioMinions
     config.load_defaults 5.2
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
