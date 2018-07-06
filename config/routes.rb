087fb63b998827f7b0ff3ced086855df1d91ca72
Natalie N. da Cunha <natalienobre@gmail.com>
2018-07-06 14:00:07 -0300

    Add help page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
+  get '/help' => 'static_pages#help'
+
 # Example of regular route:
 # get 'products/:id' => 'catalog#view'
 
