74c8152a520c690a804b4b81f9e5c35971f31640
Natalie N. da Cunha <natalienobre@gmail.com>
2018-07-07 17:21:15 -0300

    Include initialize on precompile on application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,9 @@ module DesafioMinions
     config.load_defaults 5.2
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    
+
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
