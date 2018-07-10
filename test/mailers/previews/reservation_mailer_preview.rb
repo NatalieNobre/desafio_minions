d8f391cd3b0039c7f816df24835b816b1cc008ec
Natalie N. da Cunha <natalienobre@gmail.com>
2018-07-10 20:07:52 -0300

    Mailer previews and edit email text

--- test/mailers/previews/reservation_mailer_preview.rb
+++ test/mailers/previews/reservation_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/reservation_mailer
 class ReservationMailerPreview < ActionMailer::Preview
-
+  def reservation_email
+    ReservationMailer.with(reservation: Reservation.first).reservation_email
+  end
 end
