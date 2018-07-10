9fc42f4a850ec32f18ff495d8af6b0fcfce305b7
Natalie N. da Cunha <natalienobre@gmail.com>
2018-07-09 22:13:42 -0300

    Add resources :reservations to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
 # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
+  resources :reservations, only: [:new, :create]
+
   get '/help' => 'static_pages#help'
 
-#  get '/new' => 'reservations#new'
+  get '/new' => 'reservations#new'
+
+  get '/create' => 'reservations#create'
 
 # Example of regular route:
 # get 'products/:id' => 'catalog#view'
