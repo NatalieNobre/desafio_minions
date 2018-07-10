5183454bf179e4ec783c30f26a997d2a7f952b49
Natalie N. da Cunha <natalienobre@gmail.com>
2018-07-09 21:17:19 -0300

    Redo form reservations, fixed error

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,23 +1,42 @@
-class Checkbox
+class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+  end
+
   def create
-    @flag001 = params[:flag001]
-    @flag002 = params[:flag002]
-    @flag003 = params[:flag003]
-    @flag004 = params[:flag004]
-    @flag005 = params[:flag005]
-    @flag006 = params[:flag006]
-    @flag007 = params[:flag007]
-    @flag008 = params[:flag008]
-    @flag009 = params[:flag009]
-    @flag010 = params[:flag010]
-    @flag011 = params[:flag011]
-    @flag012 = params[:flag012]
-    @flag013 = params[:flag013]
-    @flag014 = params[:flag014]
-    @flag015 = params[:flag015]
-    @flag016 = params[:flag016]
+    @client = Client.new(params[:client])
+    if @client.save
+      redirect_to @client
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render "new"
+    end
   end
 end
+
+#Cria todas as checkboxes, mas não está completp
+
+#class Checkbox
+#  def create
+#    @flag001 = params[:flag001]
+#    @flag002 = params[:flag002]
+#    @flag003 = params[:flag003]
+#    @flag004 = params[:flag004]
+#    @flag005 = params[:flag005]
+#    @flag006 = params[:flag006]
+#    @flag007 = params[:flag007]
+#    @flag008 = params[:flag008]
+#    @flag009 = params[:flag009]
+#    @flag010 = params[:flag010]
+#    @flag011 = params[:flag011]
+#    @flag012 = params[:flag012]
+#    @flag013 = params[:flag013]
+#    @flag014 = params[:flag014]
+#    @flag015 = params[:flag015]
+#    @flag016 = params[:flag016]
+#  end
+#end
 
 #  def new
 #    @reservation = reservation.new
