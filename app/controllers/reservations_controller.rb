17ebcb1cfd863dfbac63bb44332e4b84afd497e9
Natalie N. da Cunha <natalienobre@gmail.com>
2018-07-11 00:32:46 -0300

    Comment mailer bit on controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,10 +2,10 @@ class ReservationsController < ApplicationController
   def new
     @reservation = Reservation.new
 
-    respond_to do |format|
+#    respond_to do |format|
 
-      ReservationMailer.with(reservation: @reservation).reservation_email.deliver_now
-      format.html #{ redirect_to(@reservation, notice: 'Reservation was successful.') }
+#      ReservationMailer.with(reservation: @reservation).reservation_email.deliver_now
+#      format.html #{ redirect_to(@reservation, notice: 'Reservation was successful.') }
 #      format.json { render json: @reservation, status: :created, location: @reservation }
 
     end
